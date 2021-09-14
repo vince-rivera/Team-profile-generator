b4b0a7fea56df8fe14b6168826047d0dd1f476df
Vince Rivera <riverav@me.com>
2021-09-13 20:55:25 -0500

    all employee tests green

--- __tests__/Employee.tests.js
+++ __tests__/Employee.tests.js
@@ -1,10 +1,33 @@
 const Employee = require('../lib/Employee');
 
-test('test to see if employee has all relevant information', () => {
-    const employee = new Employee('Vince R', '050', 'some_guy@gmail.com');
+test('checks class of employee to verify correct information', () => {
+    const employee = new Employee('Vince Riv',123,'vince_dude@gmail.com');
 
-    expect(employee.name).toBe('Vince R');
+    expect(employee.name).toBe('Vince Riv');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-});
+})
 
+test('checks getName function', () => {
+    const employee = new Employee('Vince Riv',123,'vince_dude@gmail.com');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+})
+
+test('checks getId function', () => {
+    const employee = new Employee('Vince Riv',123,'vince_dude@gmail.com');
+
+    expect(employee.getId()).toEqual(expect.any(Number));
+})
+
+test('checks getEmail function', () => {
+    const employee = new Employee('Vince Riv',123,'vince_dude@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+})
+
+test('checks for getRole function', () => {
+    const employee = new Employee('Vince Riv',123,'vince_dude@gmail.com');
+
+    expect(employee.getRole()).toBe('Employee');
+})
