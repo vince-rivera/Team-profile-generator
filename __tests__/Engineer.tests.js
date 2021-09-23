d943f6a3eb83fdbaf3cd91558c65b14183ee9b02
Vince Rivera <riverav@me.com>
2021-09-22 20:20:04 -0500

    wrote other tests to check for functions to accept string and role to be engineer

--- __tests__/Engineer.tests.js
+++ __tests__/Engineer.tests.js
@@ -5,3 +5,21 @@ test('check to see if engineer object is created', () => {
 
     expect(engineer.name).toBe('Not Vince');
 });
+
+test('check if github is input', () => {
+    const engineer = new Engineer('Not Vince', 753, 'notvince@gmail.com', 'itnotvincesgithub');
+
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+test('check getRole() function', () => {
+    const engineer = new Engineer('Not Vince', 753, 'notvince@gmail.com', 'itnotvincesgithub');
+
+    expect(engineer.getRole()).toBe('Engineer');
+})
+
+test('checks getGitHub() function', () => {
+    const engineer = new Engineer('Not Vince', 753, 'notvince@gmail.com', 'itnotvincesgithub');
+
+    expect(engineer.getGitHub()).toEqual(expect.any(String));
+})
